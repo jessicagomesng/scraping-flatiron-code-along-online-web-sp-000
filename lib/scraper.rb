cd8619b2a3812dbba0e890db44fb41c4c7ec0249
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-08 12:03:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,6 +23,8 @@ class Scraper
     doc.css(".post").each do |post|
       course = Course.new
       course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      
 
   end
 
